bfab14d6a147bc31d0ec96a778c04a4ad29e168b
murriae <abby.murri@gmail.com>
2021-10-28 13:40:35 -0600

    adding filmList

--- films/sw-films.js
+++ films/sw-films.js
@@ -1,5 +1,11 @@
 import {films} from '../data/films.js'
 
-console.log('Welcome to the console!')
+//console.log('Welcome to the console!')
 
-console.log(films)
+//console.log(films)
+
+let filmList = document.querySelector('#filmList')
+
+console.log(filmList)
+
+filmList.textContent = "This is my content. Here is some more."
