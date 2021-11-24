18aa2235b7eea8b29e1364c051eccc7ab370a947
murriae <abby.murri@gmail.com>
2021-11-23 18:51:36 -0700

    Finished-congress

--- congress/congress.js
+++ congress/congress.js
@@ -1,17 +1,21 @@
-import { senators } from '../data/senators.js'
-import { representatives } from '../data/representatives.js'
+import { senators } from "../data/senators.js";
+import { representatives } from "../data/representatives.js";
 
-const members = [...senators, ...representatives] // modern combining arrays like a genus
+const members = [...senators, ...representatives]; // modern combining
+console.log(members.length);
 
-const senatorDiv = document.querySelector('.senators')
-const seniorityHeading = document.querySelector('.seniority')
-const weaselOrderedList = document.querySelector('.weaselList')
+const senatorDiv = document.querySelector(".senators");
+const seniorityHeading = document.querySelector(".seniority");
+const weaselOrderedList = document.querySelector(".weaselList");
+const loyaltyHeading = document.querySelector(".mostLoyal");
 
 function simplifiedMembers(chamberFilter) {
-  const filteredArray = members.filter(member => chamberFilter ? member.short_title === chamberFilter : member)
+  const filteredArray = members.filter((member) =>
+    chamberFilter ? member.short_title === chamberFilter : member
+  );
 
-  return filteredArray.map(senator => {
-    const middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
+  return filteredArray.map((senator) => {
+    const middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `;
     return {
       id: senator.id,
       name: `${senator.first_name}${middleName}${senator.last_name}`,
@@ -21,56 +25,78 @@ function simplifiedMembers(chamberFilter) {
       seniority: +senator.seniority,
       missedVotesPct: senator.missed_votes_pct,
       loyaltyPct: senator.votes_with_party_pct,
-    }
-  })
+    };
+  });
 }
 
-populateSenatorDiv(simplifiedMembers())
+populateSenatorDiv(simplifiedMembers());
 
 function populateSenatorDiv(simpleSenators) {
-  simpleSenators.forEach(senator => {
-    let senFigure = document.createElement('figure')
-    let figImg = document.createElement('img')
-    let figCaption = document.createElement('figcaption')
+  simpleSenators.forEach((senator) => {
+    let senFigure = document.createElement("figure");
+    let figImg = document.createElement("img");
+    let figCaption = document.createElement("figcaption");
 
-    figImg.src = senator.imgURL
+    figImg.src = senator.imgURL;
 
-    figCaption.textContent = senator.name
-    senFigure.appendChild(figImg)
-    senFigure.appendChild(figCaption)
-    senatorDiv.appendChild(senFigure)
-  })
+    figCaption.textContent = senator.name;
+    senFigure.appendChild(figImg);
+    senFigure.appendChild(figCaption);
+    senatorDiv.appendChild(senFigure);
+  });
 }
 
+//
 //const filterSenators = (prop, value) => simplifiedSenators().filter(senator => senator[prop] === value)
-  
+
 //const republicans = filterSenators('party', 'R')
 //const femaleSenators = filterSenators('gender', 'F')
 
 //console.log(republicans, femaleSenators)
 
-const mostSeniorMember = simplifiedMembers().reduce((acc, senator) => {
-  return acc.seniority > senator.seniority ? acc : senator 
-})
+const mostSeniorMember = simplifiedMembers('Sen.').reduce((acc, senator) => {
+  return acc.seniority > senator.seniority ? acc : senator;
+});
 
-seniorityHeading.textContent = `The most senior member of Congress is ${mostSeniorMember.name} who has taken our tax dollars as salary for more than ${mostSeniorMember.seniority} years!`
+console.log(mostSeniorMember);
+
+seniorityHeading.textContent = `The most senior member of The Senate is ${mostSeniorMember.name} who has taken our tax dollars as salary for more than ${mostSeniorMember.seniority} years!`;
 
 const mostLoyal = simplifiedMembers().reduce((acc, senator) => {
-  if(senator.loyaltyPct === 100) {
-    acc.push(senator)
+  if (senator.loyaltyPct === 100) {
+    acc.push(senator);
   }
-  return acc
-}, [])
+  return acc;
+}, []);
 
-const biggestWeasel = simplifiedMembers().reduce((acc, senator) => 
-(acc.missedVotesPct || 0) > senator.missedVotesPct ? acc : senator, {})
+/*const cowardList = document.createElement("ol");
 
-const biggestWeasels = simplifiedMembers().filter(senator => senator.missedVotesPct >= 50)
+const spineless = mostLoyal.map((coward) => {
+  let listItem = document.createElement("li");
+  listItem.textContent = coward.name;
+  cowardList.appendChild(listItem);
+});
+loyaltyHeading.appendChild(cowardList)
+loyaltyHeading.textContent =
+  "The most spineless of Congress who vote with their party 100% of the time are: ";
+console.log(mostLoyal);
 
-console.log(biggestWeasels)
+populateSenatorDiv(simplifiedMembers);
+*/
+const biggestWeasel = simplifiedMembers().reduce(
+  (acc, senator) =>
+    (acc.missedVotesPct || 0) > senator.missedVotesPct ? acc : senator,
+  {}
+);
 
-biggestWeasels.forEach(weasel => {
-  let listItem = document.createElement('li')
-  listItem.textContent = weasel.name
-  weaselOrderedList.appendChild(listItem)
-})
+const biggestWeasels = simplifiedMembers().filter(
+  (senator) => senator.missedVotesPct >= 50
+);
+
+console.log(biggestWeasels);
+
+biggestWeasels.forEach((weasel) => {
+  let listItem = document.createElement("li");
+  listItem.textContent = weasel.name;
+  weaselOrderedList.appendChild(listItem);
+});
