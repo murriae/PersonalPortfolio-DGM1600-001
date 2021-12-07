73615ace6a0f2418ba54f8ba7902648f7da4edbd
murriae <abby.murri@gmail.com>
2021-12-07 13:01:15 -0700

    updated poke cards

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -8,7 +8,14 @@ function getAPIData(url) {
   }
 }
 
-function loadPokemon(offset = 0, limit = 25) {
+/*  option1: get a pokemon by id or name
+ first, prompt the user for the name or id of the pokemon
+ second, use the getAPIData function and pass it a url like
+ `https://pokeapi.co/api/v2/pokemon/${promptResult}` 
+ third, populatePokeCard with the returned pokemon data*/
+
+
+function loadPokemon(offset = 100, limit = 25) {
   getAPIData(
     `https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offset}`,
   ).then(async (data) => {
@@ -89,10 +96,24 @@ function populateCardFront(pokemon) {
     pokeImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`
   }
   const pokeCaption = document.createElement('figcaption')
-  pokeCaption.textContent = `${pokemon.id} ${pokemon.name}`
+  pokeCaption.textContent = `${pokemon.name}`
   pokeFront.appendChild(pokeImg)
   pokeFront.appendChild(pokeCaption)
+
+  typesBackground(pokemon, pokeFront)
+
   return pokeFront
+}
+
+function typesBackground(pokemon, card) {
+  let pokeType1 = pokemon.types[0].type.name
+  let pokeType2 = pokemon.types[1]?.type.name
+  if (!pokeType2) {
+    card.style.setProperty('background', getPokeTypeColor(pokeType1))
+  } else {
+    card.style.setProperty('background',
+    `linear-gradient(${getPokeTypeColor(pokeType1)}, ${getPokeTypeColor(pokeType2)})`)
+  }
 }
 
 function populateCardBack(pokemon) {
@@ -106,8 +127,15 @@ function populateCardBack(pokemon) {
     abilityItem.textContent = ability.ability.name
     abilityList.appendChild(abilityItem)
   })
+  const pokeTypes = document.createElement('ol')
+  pokemon.types.forEach((pokeType) => {
+    let typeItem = document.createElement('li')
+    typeItem.textContent = pokeType.type.name
+    pokeTypes.appendChild(typeItem)
+  })
   pokeBack.appendChild(label)
   pokeBack.appendChild(abilityList)
+  pokeBack.appendChild(pokeTypes)
   return pokeBack
 }
 
@@ -119,4 +147,46 @@ class Pokemon {
       (this.weight = weight),
       (this.abilities = abilities)
   }
+}
+
+function getPokeTypeColor(pokeType) {
+  let color
+  switch (pokeType) {
+    case 'grass':
+      color = '#00ff00'
+      break
+      case 'fire':
+      color = '#ff0000'
+      break
+      case 'water':
+      color = '#0000ff'
+      break
+      case 'bug':
+      color = '#7fff00'
+      break
+      case 'normal':
+      color = '#f5f5dc'
+      break
+      case 'flying':
+      color = '#00ffff'
+      break
+      case 'poison':
+      color = '#c300ff'
+      break
+      case 'electric':
+      color = '#c8ff00'
+      break
+      case 'psychic':
+      color = '#e96c95'
+      break
+      case 'ground':
+      color = '#ceb250'
+      break
+      case 'rock':
+      color = '#444444'
+      break
+      default:
+        color = '#999999'
+  }
+  return color
 }
